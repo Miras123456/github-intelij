99accd21f5ae52c094347cc4c147ca56e874e64d
мирас <250174@astanait.edu.kz>
2025-12-17 13:36:04 +0500

    week2

---
+++ GYM-week2/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {  // IT-2502 Rinatuly Miras
+    public static void main(String[] args) {
+        Member member1 = new Member(001, "Birzhan", 18, "Active");
+        Trainer trainer1 = new Trainer(001, "Gani", 7, "Fitness");
+        WorkoutSession workoutSession1 = new WorkoutSession(001, "Birzhan", "Gani", 500);
+
+
+        System.out.println(member1.toString());
+        System.out.println("Active?" + member1.isActive());
+
+        member1.upgrade();
+        System.out.println("After uprgare: " + member1);
+        System.out.println("Active? " + member1.isActive());
+
+        System.out.println(trainer1.toString());
+        System.out.println("Experienced: " + trainer1.isExperienced());
+        System.out.println("Can teach: " + trainer1.canTeach());
+
+        System.out.println(workoutSession1.toString());
+        System.out.println(workoutSession1.extend());
+        System.out.println(workoutSession1.complete());
+    }
+
+}
---
+++ LearnJava/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Inventory inventory = new Inventory();
+        String string = "string";
+
+        System.out.println("HELL YEAH");
+    }
+ }
