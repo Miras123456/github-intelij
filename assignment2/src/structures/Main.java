19ddce1322aea39c2c9d179b5b627122c3859ad0
мирас <250174@astanait.edu.kz>
2026-04-07 23:18:00 +0500

    week4

--- assignment2/src/structures/Main.java
+++ assignment2/src/structures/Main.java
@@ -7,13 +7,13 @@ public class Main {
 
         while (true) {
             System.out.println("\n===== BANK MENU =====");
-            System.out.println("1. Add Account");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Show Accounts");
-            System.out.println("5. Show Last Transaction");
-            System.out.println("6. Undo Transaction");
-            System.out.println("7. Exit");
+            System.out.println("1. Add Account🏦");
+            System.out.println("2. Deposit💸");
+            System.out.println("3. Withdraw❌");
+            System.out.println("4. Show Accounts💪🏿");
+            System.out.println("5. Show Last Transaction💫");
+            System.out.println("6. Undo Transaction❌");
+            System.out.println("7. Exit👎🏿");
             System.out.print("Choose option: ");
             int c = bank.sc.nextInt();
 
