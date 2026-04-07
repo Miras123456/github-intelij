f96bf689a409995fd211e535e3a4aae626fb70eb
Miras123456 <250174@astanait.edu.kz>
2026-04-07 23:19:24 +0500

    Delete LearnJava directory

--- LearnJava/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Inventory inventory = new Inventory();
-        String string = "string";
-
-        System.out.println("HELL YEAH");
-    }
- }
